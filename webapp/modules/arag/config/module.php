2e64432d5238d1878785865994c98b7c9ea88422
Armen Baghumian <armen@opensourceclub.org>
2007-10-08 17:00:47 +0000

    module.php added for arag module.

---
+++ webapp/modules/arag/config/module.php
@@ -0,0 +1,12 @@
+<?php
+
+$module['enabled']     = False;
+$module['title']       = 'Arag';
+$module['version']     = '1.0';
+$module['name']        = 'arag';
+$module['author']      = 'Armen Baghumian (Armen@OpenSourceClub.org)';
+$module['homepage']    = '';
+$module['update_url']  = '';
+$module['description'] = '';
+
+?>
