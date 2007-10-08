89bd1dcb19f7117e23fda7ea450ad321f8811839
Armen Baghumian <armen@opensourceclub.org>
2007-10-08 22:04:39 +0000

    FCKeditor added to get_third_party.

--- etc/scripts/get_third_party.sh
+++ etc/scripts/get_third_party.sh
@@ -3,6 +3,7 @@
 
 BASE_PATH="`pwd`/`dirname $0`"
 LIBS_PATH="${BASE_PATH}/../../libs/"
+PUB_PATH="${BASE_PATH}/../../public_html/"
 
 # Check for required commands
 for CMD in wget tar unzip; do
@@ -32,7 +33,7 @@ if [ -d $LIBS_PATH/ci ]; then
 fi
 
 wget -c -P /tmp http://codeigniter.com/downloads/CodeIgniter_1.5.4.zip
-unzip /tmp/CodeIgniter_1.5.4.zip
+unzip -o /tmp/CodeIgniter_1.5.4.zip
 
 mv ./CodeIgniter_1.5.4/system ${LIBS_PATH}/ci
 
@@ -45,6 +46,26 @@ wget -c -P /tmp http://pub.opensourceclub.org/pear.tar.bz2
 tar xvfj /tmp/pear.tar.bz2
 mv ./pear $LIBS_PATH
 
+# Download FCKeditor
+
+if [ -d $PUB_PATH/scripts/FCKeditor ]; then
+    rm -rf "${PUB_PATH}/scripts/FCKeditor"
+fi
+
+wget -c -P /tmp http://prdownloads.sourceforge.net/fckeditor/FCKeditor_2.4.3.tar.gz?download
+tar xvfz /tmp/FCKeditor_2.4.3.tar.gz
+
+find ./fckeditor | grep -e '/_[a-z_0-9.]*$' | xargs rm -rf
+find ./fckeditor/ -maxdepth 1 -name fckeditor.* | grep -v 'js' | grep -v 'php' | xargs rm
+find ./fckeditor/ -type f | xargs grep ' xmlns="http:\/\/www.w3.org\/1999\/xhtml"' -l | xargs sed -i -e 's/ xmlns="http:\/\/www.w3.org\/1999\/xhtml"//'
+find ./fckeditor/ -type f | xargs grep '<meta.*/>' -l | xargs sed -i -e 's/<meta\(.*\)\/>/<meta\1>/'
+
+# Add comment tab for script tags
+sed -i -e 's/<script.*>/&\r\n\t<!--/'    ./fckeditor/editor/fckeditor.html
+sed -i -e 's/<\/script>/\/\/-->\r\n\t&/' ./fckeditor/editor/fckeditor.html
+
+mv ./fckeditor ${PUB_PATH}/scripts/FCKeditor
+
 # Cleanup
 
 rm -rf ./Smarty-2.6.18/
@@ -54,3 +75,5 @@ rm -rf ./CodeIgniter_1.5.4
 rm -rf /tmp/CodeIgniter_1.5.4.zip
 
 rm -rf /tmp/pear.tar.bz2
+
+rm -rf /tmp/FCKeditor_2.4.3.tar.gz
