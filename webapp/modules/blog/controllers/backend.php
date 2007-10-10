725b82ee8914d071fa01a2205890b5fe46179266
Armen Baghumian <armen@opensourceclub.org>
2007-10-10 15:06:44 +0000

    Initial I18N for blog module.

--- webapp/modules/blog/controllers/backend.php
+++ webapp/modules/blog/controllers/backend.php
@@ -21,12 +21,12 @@ class Backend extends Arag_Controller
 
         // Global tabbedbock
         $this->load->component('TabbedBlock', 'global_tabs');
-        $this->global_tabs->setTitle('Blog');
-        $this->global_tabs->addItem('Edit Entries', 'blog/backend/index');
-        $this->global_tabs->addItem('New Entry', 'blog/backend/post');
-        $this->global_tabs->addItem('Categories', 'blog/backend/categories');
-        $this->global_tabs->addItem('Settings', 'blog/backend/settings');
-        $this->global_tabs->addItem('Preview', 'blog/frontend');
+        $this->global_tabs->setTitle(_("Blog"));
+        $this->global_tabs->addItem(_("Edit Entries"), 'blog/backend/index');
+        $this->global_tabs->addItem(_("New Entry"), 'blog/backend/post');
+        $this->global_tabs->addItem(_("Categories"), 'blog/backend/categories');
+        $this->global_tabs->addItem(_("Settings"), 'blog/backend/settings');
+        $this->global_tabs->addItem(_("Preview"), 'blog/frontend');
     }
     // }}}
     // {{{ index
@@ -38,13 +38,14 @@ class Backend extends Arag_Controller
 
         $this->entries->setResource($this->BlogModel->getEntries());
         $this->entries->setLimit(2);
-        $this->entries->addColumn('subject', 'Subject');        
-        $this->entries->addColumn('author', 'Author');
-        $this->entries->addColumn('BlogModel.getDate', 'Create Date', PList::VIRTUAL_COLUMN);
+        $this->entries->addColumn('subject', _("Subject"));        
+        $this->entries->addColumn('author', _("Author"));
+        $this->entries->addColumn('BlogModel.getDate', _("Create Date"), PList::VIRTUAL_COLUMN);
         $this->entries->addAction('blog/backend/edit/#id#', 'Edit', 'edit_action');
         $this->entries->addAction('blog/backend/delete/#id#', 'Delete', 'delete_action');
         $this->entries->addAction('blog/backend/edit', 'Edit', 'edit_action', PList::GROUP_ACTION);
         $this->entries->addAction('blog/backend/delete', 'Delete', 'delete_action', PList::GROUP_ACTION);
+        $this->entries->setEmptyListMessage(_("There is no entry!"));
 
         $this->load->view('index');
     }
