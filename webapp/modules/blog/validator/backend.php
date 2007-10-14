091e288d6eacaf36c3256a987907ca5238bad1e3
Armen Baghumian <armen@opensourceclub.org>
2007-10-14 03:31:31 +0000

    Sample error messages are added.

--- webapp/modules/blog/validator/backend.php
+++ webapp/modules/blog/validator/backend.php
@@ -1,6 +1,7 @@
 <?php
 
-$validator['post']['write']['rules']          = Array ('subject' => 'trim|required|min_length[5]|max_length[12]|xss_clean');
-$validator['post']['write']['error_messages'] = Array ('subject' => 'Subject is required.');
+$validator['post']['write']['rules']          = Array ('subject'    => 'trim|required|min_length[5]|max_length[12]|xss_clean');
+$validator['post']['write']['error_messages'] = Array ('required'   => '%s is required.', 
+                                                       'min_length' => 'minimum length of %s is %d');
 
 ?>
