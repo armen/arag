4a19314004b5fbbfb9ba78e6b683331de8a6559a
Armen Baghumian <armen@opensourceclub.org>
2007-10-17 18:39:11 +0000

    Configuration model implemented.

---
+++ webapp/modules/core/config/module.php
@@ -0,0 +1,12 @@
+<?php
+
+$module['enabled']     = True;
+$module['title']       = 'Core';
+$module['version']     = '0.1';
+$module['name']        = 'Core';
+$module['author']      = 'Armen Baghumian (Armen@OpenSourceClub.org)';
+$module['homepage']    = '';
+$module['update_url']  = '';
+$module['description'] = '';
+
+?>
