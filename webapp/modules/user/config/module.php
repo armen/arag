29376f2caa03a17b3bac21129d22255f57209dca
Armen Baghumian <armen@opensourceclub.org>
2007-10-18 18:09:55 +0000

    Initial version of user module imported. user module database schemas are added.

---
+++ webapp/modules/user/config/module.php
@@ -0,0 +1,12 @@
+<?php
+
+$module['enabled']     = True;
+$module['title']       = 'User';
+$module['version']     = '0.1';
+$module['name']        = 'User';
+$module['author']      = 'Armen Baghumian (Armen@OpenSourceClub.org)';
+$module['homepage']    = '';
+$module['update_url']  = '';
+$module['description'] = '';
+
+?>
