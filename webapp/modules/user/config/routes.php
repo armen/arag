6439f4576b1a903b058ab3068b3a3358578b784c
Armen Baghumian <armen@opensourceclub.org>
2007-10-25 14:27:17 +0000

    routers.php added to user module.

---
+++ webapp/modules/user/config/routes.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$route['user']              = "user/backend/applications";
+$route['user/applications'] = "user/backend/applications";
+$route['user/groups']       = "user/backend/groups";
+$route['user/users']        = "user/backend/users";
+
+?>
