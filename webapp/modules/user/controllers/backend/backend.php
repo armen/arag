c80da960dbf657f5949d58a107048f1a3ff972b9
Sasan Rose <sasan.rose@gmail.com>
2007-10-25 17:32:49 +0000

    Controller Extended

--- webapp/modules/user/controllers/backend/backend.php
+++ webapp/modules/user/controllers/backend/backend.php
@@ -13,22 +13,23 @@ class Backend extends Arag_Controller
     {
         parent::Arag_Controller();
 
-        // Load the model
-        $this->load->model('UserManagementModel');        
-       
-        // Backend decorator
-        $this->load->decorator('backend/decorator');
+        // Load the models
+        $this->load->model('ApplicationsModel');        
+        $this->load->model('GroupsModel');        
+        $this->load->model('UsersModel');        
+
+        // Load URL helper
+        $this->load->helper('url');
 
         // Default page title
         $this->load->vars(Array('page_title' => 'User Management'));
 
-        // Global tabbedbock
-        $this->load->component('TabbedBlock', 'global_tabs');
-        $this->global_tabs->setTitle(_("User Management"));
-        $this->global_tabs->addItem(_("Apllications"), 'user/backend/applications');
-        $this->global_tabs->addItem(_("Groups"), 'user/backend/groups');
-        $this->global_tabs->addItem(_("Users"), 'user/backend/users');
-        $this->global_tabs->addItem(_("Settings"), 'user/backend/settings');
+        // load Clobal Tabs
+        $this->load->component('TabbedBlock', 'global_tabs'); 
+
+        // Backend decorator
+        $this->load->decorator('backend/decorator');
+
     }
     // }}}
 }
