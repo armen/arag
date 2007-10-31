d14443b2a2abb41594b36f515192927318ec6fb8
Sasan Rose <sasan.rose@gmail.com>
2007-10-31 22:05:51 +0000

    Validator extended

--- webapp/modules/user/validator/backend/applications.php
+++ webapp/modules/user/validator/backend/applications.php
@@ -11,7 +11,9 @@ $validator['error_messages'] = Array ('required'          => _("%s is required")
                                       'alpha_dash'        => _("%s can contain only alpha-numeric characters, underscores or dashes"),
                                       'alpha'             => _("%s can contain only alpha characters"),
                                       'valid_email'       => _("Please enter a valid email address"),
-                                      'min_length'        => _("%s should be at least %d characters"));
+                                      'min_length'        => _("%s should be at least %d characters"),
+                                      '_check_filter'     => _("Please enter a valid %s"),
+                                      '_check_privilege'  => _("Please enter a valid %s"));
 
 /*
  * Validation of index method
@@ -36,6 +38,31 @@ $validator['new_group']['write']['fields'] = $_new_group_fields;
 $validator['new_group']['write']['rules']  = $_new_group_rules;
 
 /*
+ * Validation of new and edit Label method
+ */
+
+$_new_label_fields = Array ('newlabel'  => _("Name for new label"),
+                            'privilege' => _("Privilege for new label"));
+
+$_new_label_rules  = Array ('newlabel'  => 'trim|required',
+                            'privilege' => 'trim|required|callback__check_privilege');
+
+$_edit_label_fields = Array ('label'     => _("Label name"),
+                             'privilege' => _("Privilege"));
+
+$_edit_label_rules  = Array ('label'     => 'trim|required',
+                             'privilege' => 'trim|required|callback__check_privilege');
+
+$validator['privileges_parents']['write']['fields'] = $_new_label_fields;
+$validator['privileges_parents']['write']['rules']  = $_new_label_rules;
+
+$validator['privileges']['write']['fields'] = $_new_label_fields;
+$validator['privileges']['write']['rules']  = $_new_label_rules;
+
+$validator['privileges_edit']['write']['fields'] = $_edit_label_fields;
+$validator['privileges_edit']['write']['rules']  = $_edit_label_rules;
+
+/*
  * Validation of new_user & user_profile method
  */
 
@@ -44,14 +71,15 @@ $_new_user_fields = Array ('username'   => _("Username"),
                            'repassword' => _("Repassword"),
                            'name'       => _("Name"),
                            'lastname'   => _("Lastname"),
-                           'email'      => _("Email"));
+                           'email'      => _("Email"),
+                           'group'      => _("Group ID"));
 
 $_new_user_rules  = Array ('name'       => 'trim|required|alpha',
                            'lastname'   => 'trim|required|alpha',
                            'email'      => 'trim|required|valid_email');
 
-$validator['new_user']['write']['fields']  = $_new_user_fields;
-$validator['new_user']['write']['rules'] = array_merge(array('username' => 'trim|required|alpha_dash|callback__check_user_name|min_length[4]'),
+$validator['new_user']['write']['fields'] = $_new_user_fields;
+$validator['new_user']['write']['rules']  = array_merge(array('username' => 'trim|required|alpha_dash|callback__check_user_name|min_length[4]'),
                                                        array('password' => 'trim|required|matches[repassword]|min_length[4]', 'repassword' => 'trim|required'),                                                       
                                                        $_new_user_rules);
 
@@ -61,6 +89,16 @@ $validator['user_profile']['write']['rules']  = array_merge(array('password'   =
 
 
 /*
+ * Validation of add and edit filters method
+ */
+
+$validator['filters_edit']['write']['fields'] = array('filter' => 'Filter');
+$validator['filters_edit']['write']['rules']  = array('filter' => 'required|callback__check_filter');
+
+$validator['app_filters']['write']['fields'] = array('filter' => 'Filter');
+$validator['app_filters']['write']['rules']  = array('filter' => 'required|callback__check_filter');
+
+/*
  * Validation of Settings method
  */
 
@@ -68,8 +106,19 @@ $_settings_fields = Array ('limit' => _("Limit"));
 
 $_settings_rules = Array ('limit' => 'trim|numeric');
 
-$validator['settings']['write']['fields']         = $_settings_fields;
-$validator['settings']['write']['rules']          = $_settings_rules;
+$validator['settings']['write']['fields'] = $_settings_fields;
+$validator['settings']['write']['rules']  = $_settings_rules;
+
+/*
+ * Validation of Add Apps method
+ */
+
+$_add_apps_fields = Array ('appname' => _("application Name"));
+
+$_add_apps_rules = Array ('appname'  => 'trim|required');
+
+$validator['add_apps_filters']['write']['fields'] = $_add_apps_fields;
+$validator['add_apps_filters']['write']['rules']  = $_add_apps_rules;
 
 /*
  * validation of read methods
@@ -94,4 +143,7 @@ $validator['new_user']['read']['rules']  = Array(1 => 'required|callback__check_
 
 $validator['user_profile']['read']['fields'] = Array(1 => 'username');
 $validator['user_profile']['read']['rules']  = Array(1 => 'required|callback__check_user_name_profile');
+
+$validator['privileges_edit']['read']['fields'] = Array(1 => 'id');
+$validator['privileges_edit']['read']['rules']  = Array(1 => 'required|callback__check_label');
 ?>
