9ee1aea3a498a2cded1332b9fb7e643ed6ac3e94
Sasan Rose <sasan.rose@gmail.com>
2007-11-15 16:33:01 +0000

    Some extra routs removed

--- webapp/modules/user/config/routes.php
+++ webapp/modules/user/config/routes.php
@@ -5,6 +5,4 @@ $config = array
     'user'              => 'user/backend/applications',
     'user/applications' => 'user/backend/applications',
     'user/application'  => 'user/backend/application',    
-    'user/groups'       => 'user/backend/groups',
-    'user/users'        => 'user/backend/users'
 );
