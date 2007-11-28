af4c232a14c4791ac0d338048cb5ff91ff1eaa29
Sasan Rose <sasan.rose@gmail.com>
2007-11-28 19:29:25 +0000

    Some settings method validations such as password removed and some like user blocking added

--- webapp/modules/multisite/validator/backend/settings.php
+++ webapp/modules/multisite/validator/backend/settings.php
@@ -6,8 +6,7 @@
 
 $validator['error_messages'] = Array (
                                        'valid_type'  => _("%s should be numeric"),
-                                       'required'    => _("%s is required"),
-                                       'valid_email' => _("%s must be a valid email address")
+                                       'required'    => _("%s is required")
                                      );
 /*
  * Validation of Limit Settings
@@ -16,26 +15,11 @@ $validator['error_messages'] = Array (
 $validator['index']['write']['rules'] = Array('limit' => array(_("Limit"), 'trim|required|numeric'));
 
 /*
- * Validation of Email Settings
+ * Validation of URI user blocking Settings
  */
 
-$validator['email']['write']['rules'] = Array(
-                                              'smtpserver' => array(_("SMTP server"), 'trim|required'),
-                                              'sender'     => array(_("Sender's email"), 'trim|required|valid_email'),
-                                              'subject'    => array(_("Subject"), 'trim|required'),
-                                              'template'   => array(_("Email's template"), 'trim|required'),
-                                              'smtpport'   => array(_("SMTP Port"), 'trim|required|numeric')
-                                             );
-
-/*
- * Validation of Password Settings
- */
-
-$validator['password']['write']['rules'] = Array('length' => array(_("Password length"), 'trim|required|numeric'));
-
-/*
- * Validation of URI expire time Settings
- */
-
-$validator['expire_time']['write']['rules'] = Array('expire' => array(_("Expire time"), 'trim|required|numeric'));
+$validator['user_blocking']['write']['rules'] = Array(
+                                                      'block_expire'  => array(_("Blocking expire time"), 'trim|required|numeric'),
+                                                      'block_counter' => array(_("Blocking attempts"), 'trim|required|numeric'),
+                                                     );
 ?>
