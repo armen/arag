fd619eea2bb1b28df9c9b225a41230b16f85cc8c
Armen Baghumian <armen@opensourceclub.org>
2007-12-12 06:14:58 +0000

    Implemeted initial version of WorkFlowEngin.

---
+++ applications/arag/libraries/WorkFlow.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Arag
+ *
+ * @package      Arag
+ * @author       Armen Baghumian
+ * @since        Version 0.1
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Component Class
+ *
+ * Components base class
+ *
+ * @package     Arag
+ * @subpackage  Libraries
+ * @author      Armen Baghumian
+ * @category    WorkFlow
+ */
+abstract class WorkFlow {
+
+    // {{{ route
+    abstract public static function route();
+    // }}}
+    // {{{ resume
+    abstract public static function resume();
+    // }}}    
+}
+
+?>
