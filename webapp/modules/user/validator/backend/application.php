cc7bd495d38f2834a105d7c100a81d72e5e0422e
Armen Baghumian <armen@opensourceclub.org>
2008-02-07 15:34:09 +0000

    Cleanup.

--- webapp/modules/user/validator/backend/application.php
+++ webapp/modules/user/validator/backend/application.php
@@ -4,7 +4,6 @@
  * Error messages
  */
 
-
 $validator['error_messages'] = Array (
                                        'required'          => _("%s is required"),
                                        '_check_group_name' => _("This %s is not available"),
@@ -16,14 +15,14 @@ $validator['error_messages'] = Array (
                                        'valid_email'       => _("Please enter a valid email address"),
                                        'min_length'        => _("%s should be at least %d characters"),
                                        '_check_password'   => _("Please enter correct %s")
-                                      );
+                                     );
 
 
 /*
  * Validation of new_group method
  */
 
-$validator['new_group']['write']['rules'] = Array ('newgroup' => array(_("Name for new group"), '=trim|required|callback__check_group_name'));
+$validator['new_group']['write']['rules'] = Array ('newgroup' => Array(_("Name for new group"), '=trim|required|callback__check_group_name'));
 
 
 /*
@@ -31,32 +30,32 @@ $validator['new_group']['write']['rules'] = Array ('newgroup' => array(_("Name f
  */
 
 $validator['new_user']['write']['rules'] = Array (
-                                                  'username'   => array(_("Username"), '=trim|required|alpha_dash|callback__check_user_name|min_length[4]'),
-                                                  'password'   => array(_("Password"), '=trim|required|matches[repassword]|min_length[4]'),
-                                                  'repassword' => array(_("Repassword"), '=trim|required'),
-                                                  'name'       => array(_("Name"), '=trim|required|alpha'),
-                                                  'lastname'   => array(_("Lastname"), '=trim|required|alpha'),
-                                                  'email'      => array(_("Email"), '=trim|required|valid_email'),
-                                                  'group'      => array(_("Group"), '=trim|required')
+                                                  'username'   => Array(_("Username"), '=trim|required|alpha_dash|callback__check_user_name|min_length[4]'),
+                                                  'password'   => Array(_("Password"), '=trim|required|matches[repassword]|min_length[4]'),
+                                                  'repassword' => Array(_("Repassword"), '=trim|required'),
+                                                  'name'       => Array(_("Name"), '=trim|required|alpha'),
+                                                  'lastname'   => Array(_("Lastname"), '=trim|required|alpha'),
+                                                  'email'      => Array(_("Email"), '=trim|required|valid_email'),
+                                                  'group'      => Array(_("Group"), '=trim|required')
                                                  );
-
 /*
  * Validation of user_profile method
  */
 
-$validator['user_profile']['read']['rules']  = Array(array(_("Username") => 'required|callback__check_user_name_profile'));
+$validator['user_profile']['read']['rules']  = Array (Array(_("Username") => 'required|callback__check_user_name_profile'));
 $validator['user_profile']['write']['rules'] = Array (
-                                                      'username'    => array(_("Username"), 'callback__check_user_name_profile'),
-                                                      'password'    => array(_("Password"), 'matches[repassword]|min_length[4]|callback__check_password'),
-                                                      'name'        => array(_("Name"), '=trim|required|alpha'),
-                                                      'lastname'    => array(_("Lastname"), '=trim|required|alpha'),
-                                                      'email'       => array(_("Email"), '=trim|required|valid_email'),
-                                                      'group'      => array(_("Group"), '=trim|required')
+                                                      'username' => Array(_("Username"), 'callback__check_user_name_profile'),
+                                                      'password' => Array(_("Password"), 'matches[repassword]|min_length[4]|callback__check_password'),
+                                                      'name'     => Array(_("Name"), '=trim|required|alpha'),
+                                                      'lastname' => Array(_("Lastname"), '=trim|required|alpha'),
+                                                      'email'    => Array(_("Email"), '=trim|required|valid_email'),
+                                                      'group'    => Array(_("Group"), '=trim|required')
                                                      );
 
 /*
  * Validation of delete method
  */
 
-$validator['do_delete']['write']['rules'] = Array('objects' => array(_("Objects"), 'callback__check_current_deletables'));
+$validator['do_delete']['write']['rules'] = Array('objects' => Array(_("Objects"), 'callback__check_current_deletables'));
+
 ?>
