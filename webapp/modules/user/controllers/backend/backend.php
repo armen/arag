755323304c38d9a2d07619edc2e811ca7a50bc79
Roham Rafii Tehrani <roham_rafii@yahoo.com>
2008-03-01 08:10:04 +0000

    Fix a syntax error

--- webapp/modules/user/controllers/backend/backend.php
+++ webapp/modules/user/controllers/backend/backend.php
@@ -302,7 +302,7 @@ class Backend_Controller extends Controller
     // {{{ _check_group_name
     public function _check_group_name($name)
     {
-        $appname = $this->input->post('application'));
+        $appname = $this->input->post('application');
 
         return $this->Groups->hasGroup($name, $appname);
     }
