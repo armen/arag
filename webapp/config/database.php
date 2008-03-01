136f7ca8318e64a61d8474bd446a3c36a16ec90d
Armen Baghumian <armen@opensourceclub.org>
2008-03-01 08:36:35 +0000

    added escape config directive.

--- webapp/config/database.php
+++ webapp/config/database.php
@@ -1,31 +1,32 @@
 <?php
-/*
- * File: Database
- *  Database connection settings, defined as arrays, or "groups". If no group
- *  name is used when loading the database library, the group named "default"
- *  will be used.
- *  
- *  Each group can be connected to independantly, and multiple groups can be
- *  connected at once.
+/**
+ * @package  Database
+ *
+ * Database connection settings, defined as arrays, or "groups". If no group
+ * name is used when loading the database library, the group named "default"
+ * will be used.
+ *
+ * Each group can be connected to independently, and multiple groups can be
+ * connected at once.
  *
  * Group Options:
- *  show_errors   - Enable or disable database exceptions
  *  benchmark     - Enable or disable database benchmarking
  *  persistent    - Enable or disable a persistent connection
  *  connection    - DSN identifier: driver://user:password@server/database
  *  character_set - Database character set
  *  table_prefix  - Database table prefix
  *  object        - Enable or disable object results
+ *  cache         - Enable or disable query caching
+ *  escape        - Enable automatic query builder escaping
  */
 $config['default'] = array
 (
-    'show_errors'   => TRUE,
     'benchmark'     => TRUE,
     'persistent'    => FALSE,
-    'connection'    => 'mysql://root:root@localhost/arag',
-    //'connection'    => 'pdosqlite:////home/armen/projects/arag/webapp/cache/arag.v3',
+    'connection'    => 'mysql://dbuser:secret@localhost/kohana',
     'character_set' => 'utf8',
-    'table_prefix'  => 'arag_',
+    'table_prefix'  => '',
     'object'        => TRUE,
-    'cache'         => FALSE    
+    'cache'         => FALSE,
+    'escape'        => TRUE
 );
