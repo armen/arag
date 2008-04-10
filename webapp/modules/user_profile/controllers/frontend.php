458de1292087b0d155571a4181c3b4571f849650
Sasan Rose <sasan.rose@gmail.com>
2008-04-10 07:36:20 +0000

    Frontend user profile added

---
+++ webapp/modules/user_profile/controllers/frontend.php
@@ -0,0 +1,13 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker:             
+// +-------------------------------------------------------------------------+
+// | Author: Sasan Rose <sasan.rose@gmail.com>                               |
+// +-------------------------------------------------------------------------+
+// $Id$
+// ---------------------------------------------------------------------------
+
+include "backend.php";
+
+class Frontend_Controller extends Backend_Controller { }
+
+?>
