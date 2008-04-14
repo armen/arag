02f5bcd82998859c0f42992b7558b60f3af29c6f
Armen Baghumian <armen@opensourceclub.org>
2008-04-14 04:06:33 +0000

    redirect to coresponding language instead of en.

--- webapp/hooks/Arag_token.php
+++ webapp/hooks/Arag_token.php
@@ -15,7 +15,10 @@ function arag_token()
         $user_token = $input->post('arag_token');
 
         if (!empty($old_token) && count($input->post()) && $old_token !== $user_token) {
-            url::redirect('invalid_request');
+            // At this point we can't Config::item('locale.lang') because Arag_site_lang 
+            // is not executed so we use cookie to get the lang.
+            $lang = (string) cookie::get('lang');
+            url::redirect($lang.'/invalid_request');
         }
 
         $session->set('arag_token', sha1(rand().time()));
