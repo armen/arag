ac69a244fe36272b23e4d836a47a79ee537c1be3
Armen Baghumian <armen@opensourceclub.org>
2008-04-20 18:57:14 +0000

    Added a new cool plugin to check authorization in templates.

---
+++ webapp/views/smarty_plugins/block.arag_is_accessible.php
@@ -0,0 +1,39 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker:             
+// +-------------------------------------------------------------------------+
+// | Author: Armen Baghumian <armen@OpenSourceClub.org>                      |
+// +-------------------------------------------------------------------------+
+// | Smarty {arag_is_accessible}{/arag_is_accessible} block plugin           |
+// |                                                                         |
+// | Type:    block function                                                 |
+// | Name:    arag_is_accessible                                             |
+// | Purpose: shows the content if it is acceessible by user                 |
+// +-------------------------------------------------------------------------+
+// $Id$
+// ---------------------------------------------------------------------------
+
+function smarty_block_arag_is_accessible($params, $content, &$smarty)
+{
+    $uri = Null;
+ 
+    foreach ($params as $_key => $_val) {
+        switch ($_key) {
+            case 'uri':
+                $$_key = (string)$_val;
+                break;
+
+            default:
+                $smarty->trigger_error("arag_accessible: unknown attribute '$_key'");
+        }
+    }
+
+    empty($uri) AND $smarty->trigger_error("arag_accessible: uri parameter should be set.");
+
+    if (Arag_Auth::is_accessible($uri)) {
+        return $content;
+    }
+
+    return Null;
+}
+
+?>
