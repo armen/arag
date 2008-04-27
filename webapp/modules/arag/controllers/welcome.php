da31985acd3b02dee24444359a7baf6659d93dee
Armen Baghumian <armen@opensourceclub.org>
2008-04-27 05:22:44 +0000

    Cleanup

--- webapp/modules/arag/controllers/welcome.php
+++ webapp/modules/arag/controllers/welcome.php
@@ -22,7 +22,7 @@ class Welcome_Controller extends Controller
     // {{{ index
     public function index()
     {
-        $this->layout->content = new View('index');      
+        $this->layout->content = new View('index');
 
         // Set the appname
         $this->layout->content->appname = ucfirst(APPNAME);
