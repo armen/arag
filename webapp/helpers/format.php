489978f2eb1e0182c6bcc6ef80fe481da1521c8f
Armen Baghumian <armen@opensourceclub.org>
2008-04-30 07:03:06 +0000

    Added time. used - instead of / for jalali date.

--- webapp/helpers/format.php
+++ webapp/helpers/format.php
@@ -26,10 +26,11 @@ class format_Core {
         $lang = Config::item('locale.lang');
 
         if ($lang === 'en') {
-            $date = date('Y-m-d', $timestamp);
+            $date = date('Y-m-d H:i:s', $timestamp);
         } else {
             $date = date::gregorian_to_jalali((int) $timestamp);
-            $date = $date['year'].'/'.$date['month'].'/'.$date['day'];
+            $date = $date['year'].'-'.$date['month'].'-'.$date['day'];
+            $date = $date . date(' H:i:s', $timestamp);
         }
 
         return $date;    
