86bfd38d95bc82158fab47208781abcd8ccbc4a8
Armen Baghumian <armen@opensourceclub.org>
2008-05-15 18:59:09 +0000

    used format::date to format dates. closes #42

--- models/staticpages.php
+++ models/staticpages.php
@@ -82,13 +82,13 @@ class StaticPages_Model extends Model
     // {{{ getDate
     public function getDate($row)
     {
-        return date('Y-m-d H:i:s', $row['create_date']);
+        return format::date($row['create_date']);    
     }
     // }}}
     // {{{ getModifyDate
     public function getModifyDate($row)
     {
-        return date('Y-m-d H:i:s', $row['modify_date']);
+        return format::date($row['modify_date']);    
     }
     // }}}
     // {{{ checkID
