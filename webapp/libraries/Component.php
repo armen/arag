a5346adffd325056844625934afbb9a3f21089cc
Armen Baghumian <armen@opensourceclub.org>
2008-05-15 18:24:09 +0000

    Implemented multi component, closes #34

--- webapp/libraries/Component.php
+++ webapp/libraries/Component.php
@@ -23,18 +23,19 @@
 class Component_Core {
     
     // {{{ Constructor
-    public function __construct($namespace = Null)
+    public function __construct($namespace = Null, $component = Null)
     {
-        $classname  = str_replace('_component', '', strtolower(get_class($this)));
-        $name       = empty($namespace) ? $classname . Router::$module : $namespace;        
+        $classname  = str_replace('_component', '', strtolower(get_class($this)));   // component class name
+        $component  = empty($component) ? $classname : $component;                   // component is module name
+        $name       = empty($namespace) ? $component . Router::$module : $namespace;        
         $controller = Kohana::instance();        
 
         // Add component plugins directory to plugins_dir
-        $controller->smarty->plugins_dir[] = MODPATH.$classname.'/views/plugins';
+        $controller->smarty->plugins_dir[] = MODPATH.$component.'/views/plugins';
         array_unique($controller->smarty->plugins_dir);
 
         // Add template dir to secure_dir list
-        $controller->smarty->secure_dir[] = MODPATH.$classname.'/views/';
+        $controller->smarty->secure_dir[] = MODPATH.$component.'/views/';
 
         $controller->smarty->assign('_'.$classname, $name);        
         $controller->smarty->assign($name.'_namespace', $namespace);
