70709c2f823a6c27080fcee37c41cd3d8c16f64b
Armen Baghumian <armen@opensourceclub.org>
2008-05-18 11:47:36 +0000

    UTF-8 should not be supported for valid::id, closes #46

--- webapp/helpers/Arag_valid.php
+++ webapp/helpers/Arag_valid.php
@@ -20,12 +20,10 @@
 class valid extends valid_Core {
     
     // {{{ id
-    public static function id($id, $utf8 = FALSE)
+    public static function id($id)
     {
-		return ($utf8 === TRUE)
-			? (bool) preg_match('/^[\pL][\pL\pN_]++$/uD', (string) $id)
-			: (bool) preg_match('/^[a-z][a-z0-9_]++$/iD', (string) $id);    
+        return (bool) preg_match('/^[a-z][a-z0-9_]++$/iD', (string) $id);    
     }
-    // }}}
+    // }}}    
 
 } // End text
