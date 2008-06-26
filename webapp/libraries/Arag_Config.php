27e25e4e68b211b1828cfcb6f4805d2be9d41699
Armen Baghumian <armen@OpenSourceClub.org>
2008-06-26 16:02:40 +0430

    Implemented, try_kohanas_config_first.

--- webapp/libraries/Arag_Config.php
+++ webapp/libraries/Arag_Config.php
@@ -1,5 +1,5 @@
 <?php
-// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker:             
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker:
 // +-------------------------------------------------------------------------+
 // | Author: Armen Baghumian <armen@OpenSourceClub.org>                      |
 // +-------------------------------------------------------------------------+
@@ -7,7 +7,7 @@
 // ---------------------------------------------------------------------------
 
 class Arag_Config_Core
-{    
+{
     // {{{ Properties
 
     private static $tableName = 'core_configuration';
@@ -30,26 +30,38 @@ class Arag_Config_Core
             $value = serialize($value);
         } else {
             $value = addslashes(serialize($value));
-        }        
+        }
 
         if (count($query) == 0){
             $db->insert(self::$tableName, Array('name' => $name, 'namespace' => $namespace, 'value' => $value));
         } else {
-            $db->update(self::$tableName, Array('name' => $name, 'namespace' => $namespace, 'value' => $value), 
+            $db->update(self::$tableName, Array('name' => $name, 'namespace' => $namespace, 'value' => $value),
                                           Array('name' => $name, 'namespace' => $namespace));
         }
-    }        
-    // }}}    
+    }
+    // }}}
     // {{{ & get
-    public static function & get($name, $default = Null, $namespace = Null)
+    public static function & get($name, $default = Null, $namespace = Null, $try_kohana_config_first = False)
     {
-        $db     = new Database();    
-        $result = $default;
-
         if ($namespace == Null) {
             $namespace = Router::$module;
         }
-        
+
+        if ($try_kohana_config_first) {
+
+            // Change include_once to module path
+            Config::set('core.modules', array_unique(array_merge($old_include_paths, Array(MODPATH.$namespace))));
+
+            $result = Config::item($name);
+
+            if (!empty($result)) {
+                return $result;
+            }
+        }
+
+        $db     = new Database();
+        $result = $default;
+
         $db->select('value');
         $query = $db->getwhere(self::$tableName, Array('name' => $name, 'namespace' => $namespace));
 
@@ -67,28 +79,26 @@ class Arag_Config_Core
     // {{{ & get_namespace
     public static function & get_namespace($namespace = Null)
     {
-        $db     = new Database();    
+        $db     = new Database();
         $result = Array();
 
         if ($namespace == Null) {
             $namespace = Router::$module;
         }
-        
+
         $db->select('name, value');
         $query = $db->getwhere(self::$tableName, Array('namespace' => $namespace));
 
         foreach ($query->result() as $row) {
-        
+
            if (get_magic_quotes_gpc() == True) {
-               $result[$row->name] = unserialize(stripslashes($row->value));           
+               $result[$row->name] = unserialize(stripslashes($row->value));
            } else {
-               $result[$row->name] = unserialize($row->value);           
+               $result[$row->name] = unserialize($row->value);
            }
-        }        
-        
+        }
+
         return $result;
     }
     // }}}
-};
-
-?>
+}
