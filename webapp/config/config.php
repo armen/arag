c4a36952d94a53abc1668009b14285ff62593fc3
Armen Baghumian <armen@OpenSourceClub.org>
2008-06-30 10:08:58 +0430

    Disable internal cacheing.

--- webapp/config/config.php
+++ webapp/config/config.php
@@ -27,7 +27,7 @@ $config['url_suffix'] = '';
  * The internal cache stores file paths and config entries across requests and
  * can give significant speed improvements at the expense of delayed updating.
  */
-$config['internal_cache'] = 120;
+$config['internal_cache'] = 0;
 
 /**
  * Enable or disable gzip output compression. This can dramatically decrease
