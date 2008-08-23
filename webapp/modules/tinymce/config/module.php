fb8d618697dffc1291a9462a09208e0d1328b9b9
Emil Sedgh <emilsedgh@gmail.com>
2008-08-23 12:47:46 +0430

    adding initial tinymce module

---
+++ webapp/modules/tinymce/config/module.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array
+(
+    'enabled'     => True,
+    'title'       => 'Tiny MCE',
+    'version'     => '0.1',
+    'name'        => 'tinymce',
+    'author'      => 'Emil Sedgh (emilsedgh@gmail.com)',
+    'homepage'    => '',
+    'update_url'  => '',
+    'description' => 'Tiny MCE WYSIWYG Widget'
+);
