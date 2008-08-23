467ff0e85f0290b337099acfc0fd2900033a0b01
Emil Sedgh <emilsedgh@gmail.com>
2008-08-23 12:46:28 +0430

    on {arag_rte}, just create a <textarea class="rte"></textarea> not a complete FCKEditor class

--- webapp/views/smarty_plugins/function.arag_rte.php
+++ webapp/views/smarty_plugins/function.arag_rte.php
@@ -3,90 +3,24 @@
 // +-------------------------------------------------------------------------+
 // | Authors: Sasan Rose <sasan.rose@gmail.com>                              |
 // |          Armen Baghumian <armen@OpenSourceClub.org>                     |
+// |          Emil Sedgh <emilsedgh@gmail.com>                               |
 // +-------------------------------------------------------------------------+
 // | Smarty {arag_rte} function plugin                                       |
 // |                                                                         |
 // | Type:    function                                                       |
 // | Name:    arag_rte                                                       |
-// | Purpose: Generating a FCKeditor                                         |
+// | Purpose: Generating a WYSIWYG widget                                    |
 // +-------------------------------------------------------------------------+
 // $Id$
 // ---------------------------------------------------------------------------
 
 function smarty_function_arag_rte($params, &$smarty)
 {
-    $toolbar_set      = 'Default';
-    $width            = '100%';
-    $height           = '300';
-    $value            = '';
-    $language         = 'en';
-    $skin             = 'default';
-    $toolbar_expanded = true;
-
-    $pub_url = url::base();
-
-    $language = Kohana::config('locale.lang');
-
-    if (Kohana::config('arag.fckeditor_skin') != Null) {
-        $skin = Kohana::config('arag.fckeditor_skin');
-    }
-
-    if (Kohana::config('arag.fckeditor_width') != Null) {
-        $width = Kohana::config('arag.fckeditor_width');
-    }
-
-    if (Kohana::config('arag.fckeditor_height') != Null) {
-        $height = Kohana::config('arag.fckeditor_height');
-    }
-
-    if (Kohana::config('arag.fckeditor_toolbar_set') != Null) {
-        $toolbar_set = Kohana::config('arag.fckeditor_toolbar_set');
-    }
-
-    if (Kohana::config('arag.fckeditor_toolbar_expanded') === False) {
-        $toolbar_expanded = Kohana::config('arag.fckeditor_toolbar_expanded');
-    }
-
-    foreach ($params as $_key => $_val) {
-        switch ($_key) {
-            case 'name':
-            case 'value':
-            case 'toolbar_set':
-            case 'width':
-            case 'height':
-                $$_key = $_val;
-                break;
-
-            default:
-                $smarty->trigger_error("arag_rte: Unknown attribute '$_key'");
-        }
-    }
-
-    if (!isset($name)) {
+    if (!isset($params['name'])) {
        $smarty->trigger_error("arag_rte: missing 'name' attribute");
        return;
     }
-
-    require_once DOCROOT . 'scripts/FCKeditor/fckeditor.php';
-
-    $FCKeditor =& new FCKeditor($name);
-
-    $FCKeditor->Config['CustomConfigurationsPath'] = $pub_url . 'scripts/FCKeditor/fckconfig.js';
-    $FCKeditor->Config['SkinPath']                 = $pub_url . 'scripts/FCKeditor/editor/skins/' . $skin . '/';
-    $FCKeditor->Config['ToolbarStartExpanded']     = $toolbar_expanded;
-    $FCKeditor->Config['AutoDetectLanguage']       = False ;
-    $FCKeditor->Config['DefaultLanguage']          = $language;
-    $FCKeditor->Config['ContentLangDirection']     = Kohana::config('locale.languages_direction.'.$language);
-    $FCKeditor->Config['DocType']                  = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" '.
-                                                     '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-
-    $FCKeditor->BasePath   = $pub_url . 'scripts/FCKeditor/';
-    $FCKeditor->Width      = $width;
-    $FCKeditor->Height     = $height;
-    $FCKeditor->Value      = $value;
-    $FCKeditor->ToolbarSet = $toolbar_set;
-
-    return $FCKeditor->CreateHtml();
+	print '<textarea name='.$params['name'].' class="rte"></textarea>';
 }
 
 ?>
