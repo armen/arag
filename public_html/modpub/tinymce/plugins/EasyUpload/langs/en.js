7b7918c848ec40b3dada78332c2f0ffdf06c40a4
Emil Sedgh <emilsedgh@gmail.com>
2008-08-25 13:43:04 +0430

    adding EasyUpload tinymce plugin, should change the name later

---
+++ public_html/modpub/tinymce/plugins/EasyUpload/langs/en.js
@@ -0,0 +1,10 @@
+tinyMCE.addI18n('en.EasyUpload',{
+Title:"Easy Upload",
+Description:"Upload an image",
+Width:"0",
+Height:"0",
+Save:"Save",
+SelectFile:"Please Select a file",
+FileNotUploaded:"Please upload a valid file",
+FileNotValid:"Please upload an image"
+});
