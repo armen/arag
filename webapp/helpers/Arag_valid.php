ae995f953777ec51a9d966b6f88090372beec2ef
Emil Sedgh <emilsedgh@gmail.com>
2008-09-20 11:39:36 +0430

    equals validator: usage: valid::equals[foo] (checks if the input value equals with foo)

--- webapp/helpers/Arag_valid.php
+++ webapp/helpers/Arag_valid.php
@@ -1,29 +1,38 @@
 <?php
-/**
- * Arag
- *
- * @package      Arag
- * @author       Armen Baghumian
- * @since        Version 0.3
- * $Id$
- */
-
-// ------------------------------------------------------------------------
-
-/**
- * text Class
- *
- * @category    Helper
- *
- */
 
 class valid extends valid_Core {
 
+    // {{{ credit_saman_card
+    public static function credit_saman_card($number)
+    {
+        return parent::credit_card($number, 'saman');
+    }
+    // }}}
+    // {{{
+    public static function credit_card($number, $type='default') //Its just a wrapper on Kohana's credit_card validation
+    {
+        if (is_array($type)) {
+            $type = $type[0]; //if its array, get the first value of it
+        }
+        return parent::credit_card($number, $type);
+    }
+    // }}}
     // {{{ id
     public static function id($id)
     {
-			return (bool) preg_match('/^[a-z][a-z0-9_]++$/iD', (string) $id);
+        return (bool) preg_match('/^[a-z][a-z0-9_]++$/iD', (string) $id);
+    }
+    // }}}
+    // {{{ filter_pan
+    public static function filter_pan($pan)
+    {
+        return str_replace('-', Null, $pan);
+    }
+    // }}}
+    // {{{ equals
+    public static function equals($value, $options) {
+        return (bool) $value == current($options);
     }
     // }}}
 
-} // End text
+} // End valid
