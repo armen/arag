58fb3f746d3810f5517d4f332f99dab13052f405
Armen Baghumian <armen@OpenSourceClub.org>
2008-09-30 10:00:23 +0330

    New generation of extract_gettext.sh to extract all messages from all modules.

--- etc/scripts/extract_gettext.sh
+++ etc/scripts/extract_gettext.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 # vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker:
 
-if [ ! -f ./ref.po ]; then
+for path in `find . -name 'LC_MESSAGES' -type d | grep -v .svn`
+do
+    if [ ! -f ./ref.po ]; then
 
-    echo ""
-    echo "Extracting messages..."
+        echo ""
+        echo "Extracting messages..."
+        find ${path} -type f | grep -v '.svn' | grep -v -e '/\..*' | grep -v 'schemas' | grep -v 'LC_MESSAGES' | grep -e "\.(php)|(tpl)$" | xgettext --keyword=_ --force-po --files-from - --output=ref.po
 
-    find . -type f | grep -v '.svn' | grep -v -e '/\..*' | grep -v 'schemas' | grep -v 'LC_MESSAGES' | xgettext --keyword=_ --force-po --files-from - --output=ref.po
-    sed -i -e 's/CHARSET/utf-8/' ./ref.po
+        sed -i -e 's/CHARSET/utf-8/' ./ref.po
+        echo "Merging/Creating messages..."
 
-    echo ""
-    echo "Merging/Creating messages..."
+        if [ -f "${path}/messages.po" ]; then
+            echo -n "Mergeing ref.po file with ${path}/messages.po file: "
+            msgmerge "${path}/messages.po" ./ref.po -o "${path}/messages.po"
+        else
+            echo "Creating ${path}/messages.po"
+            mv ./ref.po "${path}/messages.po"
+        fi
 
-    if [ -d ./locale ]; then
-
-        for path in `find ./locale -name 'LC_MESSAGES' -type d | grep -v .svn`
-        do
-            if [ -f "${path}/messages.po" ]; then
-                echo -n "Mergeing ref.po file with ${path}/messages.po file: "
-                msgmerge "${path}/messages.po" ./ref.po -o "${path}/messages.po"
-            else
-                echo "Creating ${path}/messages.po"
-                mv ./ref.po "${path}/messages.po"
-            fi
-        done
-
-        rm -rf ref.po
+        rm -rf ./ref.po
 
     else
-        echo "Error: All messages extracted but I can't find locale directory! please create it first."
+       echo "Error: ref.po already exists, please remove old file first."
+       exit 1;
     fi
-else
-    echo "Error: ref.po already exists, please remove old file first."
-fi
+done      
