b6ca446f0d05a9015eff50556fc317e6036fc2d9
Peyman <pk@zeeg.(none)>
2009-01-25 08:52:21 +0330

    Added module.php fo logger and messaging

---
+++ webapp/modules/logger/config/module.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array
+(
+    'enabled'     => True,
+    'title'       => 'Logger',
+    'version'     => '0.1',
+    'name'        => 'logger',
+    'author'      => 'Jila khaghani (jilakhaghani@gmail.com)',
+    'homepage'    => '',
+    'update_url'  => '',
+    'description' => ''
+);
---
+++ webapp/modules/messaging/config/module.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array
+(
+    'enabled'     => True,
+    'title'       => 'Messaging',
+    'version'     => '0.1',
+    'name'        => 'messaging',
+    'author'      => 'Jila khaghani (jilakhaghani@gmail.com)',
+    'homepage'    => '',
+    'update_url'  => '',
+    'description' => ''
+);
