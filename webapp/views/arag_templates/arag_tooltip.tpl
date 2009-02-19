176e94d8038c3eac1c14cf4131c647bc9acbbd90
Emil Sedgh <emilsedgh@gmail.com>
2009-02-19 11:43:59 +0330

    new arag_tooltip

--- webapp/views/arag_templates/arag_tooltip.tpl
+++ webapp/views/arag_templates/arag_tooltip.tpl
@@ -1,39 +1,17 @@
 {arag_load_script src="scripts/mootools.js"}
-
-{if $type == 'block'}
-    <{if $inline}span{else}div{/if}{if isset($class|smarty:nodefaults)} class="{$class}"{/if}{if isset($style|smarty:nodefaults)} style="{$style}"{/if} id = "{$tip_id}">
-        {$content|smarty:nodefaults}
-    </{if $inline}span{else}div{/if}>
-    <script type="text/javascript">
-        //<![CDATA[
-        var tip        = '{$tip_id}';
-        var tip_title  = '{$tip_title|smarty:nodefaults}';
-        var tip_text   = '{$tip_text|smarty:nodefaults}';
-        var class_name = '{$tip_class}';
-        {literal}
-            $(tip).store('tip:title', tip_title);
-            $(tip).store('tip:text', tip_text);
-            var myTip = new Tips($(tip), {
+{arag_load_script src="scripts/mootools-more.js"}
+<script type="text/javascript">
+    //<![CDATA[
+    var activator  = '{$activator}';
+    var title      = "{$title|smarty:nodefaults}";
+    var content    = "{$content|smarty:nodefaults}";
+    var class_name = '{$class_name}';
+    {literal}
+        $(activator).store('tip:title', title);
+        $(activator).store("tip:text", content);
+        var myTip = new Tips($(activator), {
                 className: class_name
             });
-        {/literal}
-        //]]>
-    </script>
-{else}
-    <a href="{$href}" title="{$tip_title|smarty:nodefaults}" id="{$tip_id}">
-        {$content|smarty:nodefaults}
-    </a>
-    <script type="text/javascript">
-        //<![CDATA[
-        var tip        = '{$tip_id}';
-        var tip_text   = '{$tip_text|smarty:nodefaults}';
-        var class_name = '{$tip_class}';
-        {literal}
-            $(tip).store('tip:text', tip_text);
-            var myTip = new Tips($(tip), {
-                className: class_name
-            });
-        {/literal}
-        //]]>
-    </script>
-{/if}
+    {/literal}
+    //]]>
+</script>
--- webapp/views/smarty_plugins/block.arag_tooltip.php
+++ webapp/views/smarty_plugins/block.arag_tooltip.php
@@ -15,23 +15,14 @@
 function smarty_block_arag_tooltip($params, $content, &$smarty, &$repeat)
 {
     if (!$repeat) {
-        $class  = Null;
-        $style  = Null;
-        $tip_class = 'tool-tip';
-        $inline = False;
-        $text   = '&nbsp;';
+        $class_name = 'tooltip';
 
         foreach ($params as $_key => $_val) {
             switch ($_key) {
-                case 'type'     :
-                case 'inline'   :
-                case 'class'    :
-                case 'id'       :
-                case 'href'     :
-                case 'style'    :
-                case 'tip_text' :
-                case 'tip_title':
-                case 'tip_class':
+                case 'activator':
+                case 'title':
+                case 'class_name':
+                case 'tooltip':
                     $$_key = $_val;
                     break;
 
@@ -39,36 +30,12 @@ function smarty_block_arag_tooltip($params, $content, &$smarty, &$repeat)
                     $smarty->trigger_error("arag_tooltip: Unknown attribute '$_key'");
             }
         }
-
-        if (!isset($id)) {
-           $smarty->trigger_error("arag_tooltip: missing 'id' attribute");
-           return Null;
-        }
-
-        if (!isset($type)) {
-           $smarty->trigger_error("arag_tooltip: missing 'type' attribute");
-           return Null;
-        } else if (strtolower($type) != 'link' && strtolower($type) != 'block') {
-           $smarty->trigger_error("arag_tooltip: '$type' is an uknown 'type'. Valid types are 'link' and 'block'");
-           return Null;
-        }
-
-        if (strtolower($type) == 'link' && !isset($href)) {
-           $smarty->trigger_error("arag_tooltip: You are using 'link' as type. Therefore you have to specify href");
-           return Null;
-        }
-
-        $smarty->assign_by_ref('content', $content);
-        $smarty->assign('type', strtolower($type));
-        $smarty->assign('tip_id', $id);
-        // If it should be a span
-        $smarty->assign('inline', $inline);
-        $smarty->assign('class', $class);
-        $smarty->assign('href', isset($href) ? $href : NULL);
-        $smarty->assign('style', $style);
-        $smarty->assign('tip_class', $tip_class);
-        $smarty->assign('tip_text', (isset($tip_text) ? $tip_text : $text));
-        $smarty->assign('tip_title', (isset($tip_title) ? $tip_title : $text));
+        $content = str_replace("\n", '', $content);
+        $content = str_replace('"', "'", $content);
+        $smarty->assign('title', $title);
+        $smarty->assign('activator', $activator);
+        $smarty->assign('content', $content);
+        $smarty->assign('class_name', $class_name);
 
         // Find template location
         $template = Kohana::find_file('views', 'arag_templates/arag_tooltip', True, 'tpl');
