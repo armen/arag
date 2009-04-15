9cca3fe4e1339f23aaf9068d7fd4bbcf14c1665c
Peyman Karimi <zeegco@yahoo.com>
2009-04-15 10:40:39 +0430

    Change way of being informed about content.

--- webapp/libraries/Arag_Controller.php
+++ webapp/libraries/Arag_Controller.php
@@ -226,7 +226,7 @@ class Controller extends Controller_Core {
     public function _display()
     {
         if ($this->layout instanceof View) {
-            isset($this->layout->content) AND $this->layout->content = $this->layout->content->render(); // We render content before main layout in order to make things
+            ($this->layout->content InstanceOf View) AND $this->layout->content = $this->layout->content->render(); // We render content before main layout in order to make things
             $this->layout->render(True);
         }
     }
