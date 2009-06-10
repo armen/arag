894643162a267295dd05665cbf5cc7693fb00392
Peyman Karimi <zeegco@yahoo.com>
2009-06-10 19:56:24 +0430

    Fix some bugs in locations script.

--- public_html/modpub/locations/locations.js
+++ public_html/modpub/locations/locations.js
@@ -10,33 +10,39 @@ var initLocations = function(country_name, province_name, city_name) {
 
     $(province_name).addEvent('change', function (e) {
         e.stop();
-        getCitiesUrl = getCitiesBaseUrl + $(province_name).getProperty('value')  + '/' + $(country_name).getProperty('value');
+        getCitiesUrl = getCitiesBaseUrl + ($(province_name).getProperty('value') ? $(province_name).getProperty('value') : 0) + '/' + $(country_name).getProperty('value');
         fetchData(getCitiesUrl, city_name);
     });
 }
 
-var fetchData = function(sourceUrl, destination) {
+var fetchData = function(sourceUrl, destination, selected) {
 
     var request = new Request.JSON({
         url: sourceUrl,
         onComplete: function(jsonObj) {
-            updateSelect(jsonObj.entries, destination);
+            updateSelect(jsonObj.entries, destination, selected);
         }
     }).send();
 }
 
-var updateSelect = function(options, name) {
+var updateSelect = function(options, name, selected) {
     var container = $(name+'_container');
     if (options.length) {
         var select = $(name);
+        var isSelected = false;
         select.empty();
+        new Element('option', {'value':'', 'html':''}).inject(select);
         options.each(function(option) {
-            new Element('option', {'value':option.key, 'html':option.value}).inject(select);
+            isSelected = (option.key == selected) ? true : false;
+            new Element('option', {'value':option.key, 'html':option.value, 'selected' : isSelected}).inject(select);
         });
         container.appendChild(select);
         new Fx.Slide(container).slideIn();
     } else {
-        $(name).setProperty('value', 0);
+        var select = $(name);
+        var isSelected = false;
+        select.empty();
+        container.appendChild(select);
         new Fx.Slide(container).slideOut();
     }
 }
