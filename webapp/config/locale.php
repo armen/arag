0c965b25ebc689b3df71deb368bcdafc2f62c37f
Emil Sedgh <emilsedgh@gmail.com>
2009-07-29 10:58:45 +0430

    better default_location

--- webapp/config/locale.php
+++ webapp/config/locale.php
@@ -35,9 +35,7 @@ $config['languages_direction'] = array
     'fa' => 'rtl'
 );
 
-$config['default_country']  = 32;
-$config['default_province'] = 7;
-$config['default_city']     = 111299;
+$config['default_location'] = 11813;
 
 $config['currency'] = array
 (
