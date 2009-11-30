794f8df8d7bd70665767909f03a1d6b05ed30c1f
Siavash Safi <siavash@siavashs.org>
2009-11-30 10:02:23 +0330

    Logged 404 errors

--- webapp/hooks/Arag_404.php
+++ webapp/hooks/Arag_404.php
@@ -4,5 +4,7 @@ Event::replace('system.404', array('Kohana', 'show_404'), 'arag_404');
 
 function arag_404()
 {
-    url::redirect('core/frontend/messages/page_not_found');
+    Kohana::log('info', 'Following controller does not exists: `'.url::current().'`');
+    (PHP_SAPI != 'cli') AND url::redirect('core/frontend/messages/page_not_found');
+    exit;
 }
