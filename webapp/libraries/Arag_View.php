20fa17eebea6cb0a188d2060a8c120ec0c7cc5e9
Sasan Rose <sasan@sasan.(none)>
2009-12-14 14:46:49 +0330

    View now can use templates in other modules

--- webapp/libraries/Arag_View.php
+++ webapp/libraries/Arag_View.php
@@ -21,7 +21,7 @@ class View extends View_Core {
     {
         $type = empty($type) ? Kohana::config('smarty.templates_ext') : $type;
 
-        if (!Kohana::find_file('views', $name, FALSE, $type)) {
+        if (!Kohana::find_file('views', $name, FALSE, $type) && !file::is_path_absolute($name)) {
             $type = Null;
         }
 
@@ -42,6 +42,21 @@ class View extends View_Core {
     public function toArray()
     {
         return array_merge(parent::$kohana_global_data, $this->kohana_local_data);
+    }
+    // }}}
+    // {{{ set_filename
+	public function set_filename($name, $type = NULL)
+	{
+        if (file::is_path_absolute($name)) {
+            $this->kohana_filename = $name;
+
+            // Use the specified type
+			($this->kohana_filetype == NULL && isset($type)) AND $this->kohana_filetype = $type;
+
+            return $this;
+        }
+
+        return parent::set_filename($name, $type);
     }
     // }}}
 }
