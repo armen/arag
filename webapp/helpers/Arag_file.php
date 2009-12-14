5eb2ba9f29e040af32f077c602cdc89c001c72cf
Sasan Rose <sasan@sasan.(none)>
2009-12-14 14:53:50 +0330

    is_absolute_path url added to the file helper

--- webapp/helpers/Arag_file.php
+++ webapp/helpers/Arag_file.php
@@ -7,5 +7,16 @@ class file extends file_Core {
         // Return the extension of the filename
         return ltrim(substr($filename, strrpos($filename, '.')), '.');
     }
+    public static function is_path_absolute($path)
+    {
+        if ($path[0] == '/' || $path[0] == '\\' ||
+            (strlen($path) > 3 && ctype_alpha($path[0]) && $path[1] == ':' && ($path[2] == '\\' || $path[2] == '/'))
+           )
+        {
+            return true;
+        }
+
+        return false;
+    }
 
 } // End file
